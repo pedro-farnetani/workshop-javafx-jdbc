eb258bc0a6ee6ecd470b02ed4c0fe8882804689f
Pedro Farnetani de Almeida <pedrofalterosa@gmail.com>
2020-07-02 19:52:32 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,8 +10,10 @@ import java.io.IOException;
 
 public class Main extends Application {
 
+    private static Scene mainScene;
+
     @Override
-    public void start(Stage primaryStage) {
+    public synchronized void start(Stage primaryStage) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
             ScrollPane scrollPane = loader.load();
@@ -19,13 +21,17 @@ public class Main extends Application {
             scrollPane.setFitToHeight(true);
             scrollPane.setFitToWidth(true);
 
-            Scene mainScene = new Scene(scrollPane);
+            mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFX application");
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Scene getMainScene() {
+        return mainScene;
     }
 
     public static void main(String[] args) {
